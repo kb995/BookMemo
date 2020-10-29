18e4564fae0e656f31da5a12960a6ffb8b678f49
kb995 <t.kuboshima995@gmail.com>
2020-10-30 06:29:01 +0900

    #10 test commit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,6 +14,7 @@ class BookController extends Controller
      */
     public function index()
     {
+        // test
         $books = Book::all()->sortByDesc('created_at');
 
         return view('books.index', compact('books'));
