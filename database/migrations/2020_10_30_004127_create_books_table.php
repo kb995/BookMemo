8d94c76fcbb8e6f303a50c1fe643c1b4b9f98410
kb995 <t.kuboshima995@gmail.com>
2020-10-30 05:43:31 +0900

    [add]booksテーブル作成

---
+++ database/migrations/2020_10_30_004127_create_books_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->string('cover')->nullable();
+            $table->string('author', 100)->nullable();
+            $table->string('isbn')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('status')->default(0);
+            $table->integer('rank')->default(0);
+            $table->date('read_at')->nullable();
+            // $table->bigInteger('user_id')->unsigned();
+            // $table->foreign('user_id')->references('id')->on('users');
+            // $table->bigInteger('category_id')->unsigned();
+            // $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
