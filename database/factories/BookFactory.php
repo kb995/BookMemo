d55fe5ddd4b35d80a808219c5dcfe81690373734
kb995 <t.kuboshima995@gmail.com>
2020-11-02 01:15:12 +0900

    [add]BoodSeeder/Factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Book;
+use Faker\Generator as Faker;
+
+$factory->define(Book::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'cover' => '',
+        'author' => $faker->name,
+        'isbn' => '123456789',
+        'description' => $faker->paragraph,
+        'status' => '',
+        'rank' => '',
+        'user_id' => 1,
+    ];
+});
