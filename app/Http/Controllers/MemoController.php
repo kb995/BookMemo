1207de1c0727d215163ae87e340dcef1038e255c
kb995 <t.kuboshima995@gmail.com>
2020-11-07 03:57:44 +0900

    [add]Memo validation

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\MemoRequest;
 use App\Models\Book;
 use App\Models\Memo;
 use App\User;
@@ -11,9 +12,11 @@ use App\User;
 class MemoController extends Controller
 {
 
-    public function store(Book $book, Memo $memo, Request $request)
+    public function store(Book $book, Memo $memo, MemoRequest $request)
     {
         $memo->memo = $request->memo;
+        $memo->book_id = $book->id;
+        $memo->save();
 
         return redirect()->route('books.show', ['book' => $book]);
     }
@@ -23,7 +26,7 @@ class MemoController extends Controller
         return view('memos.edit', compact('book', 'memo'));
     }
 
-    public function update(Request $request, Book $book, Memo $memo)
+    public function update(MemoRequest $request, Book $book, Memo $memo)
     {
         $memo->memo = $request->memo;
         $memo->save();
