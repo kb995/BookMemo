5c3d9940736e03f0f1e4de1c6c573fa541f8ec81
kb995 <t.kuboshima995@gmail.com>
2020-11-07 00:39:38 +0900

    [update]本・メモ削除処理

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -11,18 +11,6 @@ use App\User;
 class MemoController extends Controller
 {
 
-    // public function index()
-    // {
-    //     //
-    // }
-
-
-    // public function create()
-    // {
-    //     return view('memos.create');
-    // }
-
-
     public function store(Book $book, Memo $memo, Request $request)
     {
         $memo->memo = $request->memo;
@@ -30,31 +18,23 @@ class MemoController extends Controller
         return redirect()->route('books.show', ['book' => $book]);
     }
 
-
-    public function show($id)
-    {
-        //
-    }
-
-
     public function edit(Book $book, Memo $memo)
     {
         return view('memos.edit', compact('book', 'memo'));
     }
 
-
     public function update(Request $request, Book $book, Memo $memo)
     {
         $memo->memo = $request->memo;
-
         $memo->save();
 
         return redirect()->route('books.show', ['book' => $book]);
     }
 
 
-    public function destroy($id)
+    public function destroy(Book $book, Memo $memo)
     {
-        //
+        $memo->delete();
+        return redirect()->route('books.show', ['book' => $book]);
     }
 }
