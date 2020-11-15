30472b7d8cfa12c8b0d583e867265ee0f630db3f
kb995 <t.kuboshima995@gmail.com>
2020-11-15 23:53:27 +0900

    [add]メモ一覧のスタイル調整

--- resources/views/memos/create.blade.php
+++ resources/views/memos/create.blade.php
@@ -1,8 +1,8 @@
-<form method="POST" action="{{ route('books.memos.store', ['book' => $book]) }}" class="card mx-auto w-50 p-5 my-5">
+<form method="POST" action="{{ route('books.memos.store', ['book' => $book]) }}" class="card p-4 my-4">
     @csrf
     <div class="form-group">
         <label for="memo"></label>
-        <textarea class="form-control" id="memo" name="memo" value="{{ old('memo') }}" rows="4" cols="40"></textarea>
+        <textarea class="form-control" id="memo" name="memo" value="{{ old('memo') }}" rows="4" cols="40" placeholder="読書メモ"></textarea>
         <input type="submit" class="btn btn-primary my-4">
     </div>
 </form>
--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -1,23 +1,29 @@
-<article class="mx-auto w-50">
+<section>
     @foreach ($memos as $memo)
     <div class="card mb-4">
-        <div class="p-3 inline">
-            {{ $memo->id }} : {{ $memo->memo }}
-            <a class="inline" href="{{ route('books.memos.edit', ['book' => $book, 'memo' => $memo]) }}">編集</a>
+        <div class="card-header">
+            {{ $memo->id }}
+        </div>
+        <div class="card-body">
+            {{ $memo->memo }}
+        </div>
 
-            <form action="{{ route('books.memos.destroy', ['book' => $book, 'memo' => $memo]) }}" method="post" id="delete_memo_{{ $memo->id }}">
+        <div class="card-footer memo-info">
+            <a class="inline" href="{{ route('books.memos.edit', ['book' => $book, 'memo' => $memo]) }}">編集</a>
+            <form class="delete-form" action="{{ route('books.memos.destroy', ['book' => $book, 'memo' => $memo]) }}" method="post" id="delete_memo_{{ $memo->id }}">
                 @csrf
                 @method('DELETE')
                 <a class="inline text-danger" data-id="{{ $memo->id }}" onclick="deleteMemo(this);">
-                    <i class="fas fa-trash-alt pr-1"></i>
+                    <i class="fas fa-trash-alt"></i>
                     削除
                 </a>
             </form>
-
         </div>
     </div>
     @endforeach
 
-    {{ $memos->links() }}
+    <div class="text-center">
+        {{ $memos->links() }}
+    </div>
 
-</article>
+</section>
