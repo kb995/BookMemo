b357f218dbce67d818119cb1c342a0474e9de415
kb995 <t.kuboshima995@gmail.com>
2020-11-17 05:18:20 +0900

    [add]メモタグのシーダー/ファクトリー

---
+++ database/factories/MtagFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Mtag;
+use Faker\Generator as Faker;
+
+$factory->define(Mtag::class, function (Faker $faker) {
+    return [
+        'name' => 'タグ',
+    ];
+});
