c5721730fa71a7883e4b7994b36bc900dee6d4f9
kb995 <t.kuboshima995@gmail.com>
2020-11-23 21:29:20 +0900

    [add]書籍タグのモデル/マイグレーション

---
+++ database/migrations/2020_11_23_092415_create_btags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBtagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('btags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            // ->unique();
+            $table->bigInteger('book_id')->unsigned()->nullable();
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('btags');
+    }
+}
