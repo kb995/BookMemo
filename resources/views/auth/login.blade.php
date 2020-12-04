85a096455062b5a0cf7d67f01d2b0665c42ff62f
kb995 <t.kuboshima995@gmail.com>
2020-12-04 23:33:17 +0900

    [update]ログインのスタイル修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <h1 class="text-center my-5">ログイン</h1>
 
-                <div class="card-body">
+            <p class="text-center mb-3">ブクメモに登録している方は、下記からログインしてください。</br>
+                まだ登録していない方は<a href="{{ route('register') }}">コチラ</a>から登録してください。</p>
+
+            <div class="card">
+                <div class="card-body p-5">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="kanou.sayuri@example.com" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="sasaki.taro@example.net" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -41,7 +43,7 @@
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
+                                <div class="form-check text-right text-muted">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
@@ -51,20 +53,32 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                        <div class="form-group mb-0 text-center">
+                            <button type="submit" class="btn btn-outline-primary w-75 mx-auto my-3">
+                                {{ __('Login') }}
+                            </button>
+
+                            <div class="col-md-8 offset-md-4 text-right">
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link mr-5" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
+
                         </div>
                     </form>
+
+                    <p class="font-weight-bold py-4 text-center">アカウントをお持ちでない方はこちら</p>
+
+                    <div class="text-center">
+                        <button class="register-button btn w-75 mx-auto">
+                            <a class="register-link" href="{{ route('register') }}">
+                                新規会員登録
+                            </a>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
