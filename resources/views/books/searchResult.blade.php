13db53e54aea8c4e68d9d7673858f2e6f7c966a6
kb995 <t.kuboshima995@gmail.com>
2020-12-24 05:12:23 +0900

    [add]google api 書籍検索機能

---
+++ resources/views/books/searchResult.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts/layout')
+
+@section('title', '検索結果')
+
+@section('styles')
+    {{-- @include('libs.flatpickr.styles') --}}
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"> --}}
+@endsection
+
+@section('content')
+
+<section class="shelf-serach" style="border-color: blue;">
+    <form method="POST" action="{{ route('books.api') }}" class="text-center my-3">
+        @csrf
+        <div class="form-group ml-3">
+            <label class="shelf-serach-label block" for="keyword">Googleから探す</label>
+            <input class="shelf-serach-input" type="text" name="keyword" value="{{ old('keyword') }}" placeholder="キーワードで検索">
+            <input type="submit" class="shelf-serach-btn" value="検索" style="background-color: blue;">
+        </div>
+    </form>
+</section>
+
+<section class="book-shelf">
+    @foreach ($books as $book)
+    <div class="card my-4 p-5">
+
+        @if (array_key_exists('imageLinks', $book['volumeInfo']))
+        <p class="img-thumbnail d-inline">
+            <img src="{{ $book['volumeInfo']['imageLinks']['thumbnail']}}"><br>
+        </p>
+        @endif
+
+        @if (array_key_exists('title', $book['volumeInfo']))
+        <h4>{{ $book['volumeInfo']['title'] }}</h4>
+        @endif
+
+        @if (array_key_exists('authors', $book['volumeInfo']))
+            <p>著者: {{ $book['volumeInfo']['authors'][0] }}</p>
+        @endif
+
+        @if (array_key_exists('publisher', $book['volumeInfo']))
+            <p>出版社: {{ $book['volumeInfo']['publisher'] }}</p>
+        @endif
+
+        @if (array_key_exists('publishedDate', $book['volumeInfo']))
+            <p>発売年月：{{ $book['volumeInfo']['publishedDate']}}</p>
+        @endif
+
+        @if (array_key_exists('description', $book['volumeInfo']))
+            <p>概要: {{ $book['volumeInfo']['description'] }}</p>
+        @endif
+    </div>
+    @endforeach
+</section>
+
+    <div class="text-center py-1 mt-5">
+        Copyright © 2020 ***. All Rights Reserved.
+    </div>
+@endsection
