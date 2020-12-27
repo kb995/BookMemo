28d1a48f548d86027cf9904c058b01bc509123ca
kb995 <t.kuboshima995@gmail.com>
2020-12-27 23:46:08 +0900

    [update]トップページのユーザー情報スタイル更新

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -9,7 +9,44 @@
 
 @section('content')
 
-<section class="info-wrapper">
+<section class="user">
+    <div class="user-mask">
+        <div class="row w-75 mx-auto py-2">
+            {{-- サムネイル --}}
+           <div class="col-2 user-thumbnail">
+               <img class="img-thumbnail" src="../storage/app/public/common/default_user.jpeg" alt="ユーザーアイコン">
+               <p class="mt-2"><a class="user-edit-link" href="{{ route('user.edit', ['user' => Auth::user()]) }}">{{ $user->name }}さん</a></p>
+           </div>
+           {{-- 本棚名 --}}
+           <div class="col-3 text-center">
+               <h1 class="h5 user-title ml-4 mb-3 row">{{ $user->name }}の本棚</h1>
+               <div class="user-info row">
+                    <dl>
+                        <dt>登録本</dt>
+                        <dd>{{ $book_counts['books_all'] }} <small class="text-muted">冊</small></dd>
+                    </dl>
+                    <dl>
+                        <dt>読了本</dt>
+                        <dd>{{ $book_counts['books_read'] }} <small class="text-muted">冊</small></dd>
+                    </dl>
+                    <dl>
+                        <dt>積読本</dt>
+                        <dd>{{ $book_counts['books_pile'] }} <small class="text-muted">冊</small></dd>
+                    </dl>
+               </div>
+           </div>
+           <div class="col-5 d-flex align-items-center">
+            <div class="link-info">
+                <a href="{{ route('books.search') }}" class="btn btn-lg p-3 mr-2 btn-outline-primary d-inline-block"><i class="fas fa-search pr-2"></i></i>検索して本を登録</a>
+                <a href="{{ route('books.create') }}" class="btn btn-lg p-3 btn-outline-success d-inline-block"><i class="fas fa-pen pr-2"></i>入力して本を登録</a>
+            </div>
+           </div>
+        </div>
+    </div>
+</section>
+
+
+{{-- <section class="info-wrapper">
     <div class="user-info">
         <div class="row">
             @if($user->thumbnail)
@@ -69,20 +106,20 @@
         <p><a href="{{ route('books.create') }}" class="btn px-5 py-2 btn-outline-success"><i class="fas fa-pen pr-2"></i>入力して本を登録</a></p>
         <p><a href="{{ route('books.search') }}" class="btn px-5 py-2 btn-outline-primary"><i class="fas fa-pen pr-2"></i>検索して本を登録</a></p>
     </div>
-</section>
+</section> --}}
 
-<section class="shelf-serach">
-    <form method="POST" action="{{ route('books.index') }}" class="text-center my-3">
+{{-- <section class="shelf-serach"> --}}
+    {{-- <form method="POST" action="{{ route('books.index') }}" class="text-center my-3">
         @csrf
         <div class="form-group ml-3">
             <label class="shelf-serach-label block" for="keyword">本棚から探す</label>
             <input class="shelf-serach-input" type="text" name="keyword" value="{{ old('keyword') }}" placeholder="キーワードで検索">
             <input type="submit" class="shelf-serach-btn" value="検索">
         </div>
-    </form>
+    </form> --}}
 
     {{--  詳細検索フォーム  --}}
-    <div class="text-center detail-search">
+    {{-- <div class="text-center detail-search">
         <button class="btn btn-link btn-detail"
         data-toggle="collapse"
         data-target="#input"
@@ -100,7 +137,6 @@
                         <select class="detail-search-input" name="category" id="category">
                             <option default>カテゴリー選択</option>
                             @foreach ($category_list as $category)
-                            {{--  <option value="{{ $category->category }}" {{ old('category') === $category->category ? 'selected' : '' }}>  --}}
                             <option value="{{ $category->category }}">
                                 {{ $category->category }}
                             </option>
@@ -120,7 +156,6 @@
                     </div>
                     <div class="col-6">
                         <label class="detail-search-label" for="status">読書状態から探す</label>
-                        {{--  <input class="detail-search-input" name="status" id="status" placeholder="未読 or 既読 or 積読">  --}}
                         <select name="status" class="detail-search-input" id="status" value="{{ old('status') }}">
                             <option value="0" {{ old('status') === '0' ? 'selected' : '' }}>-</option>
                             <option value="1" {{ old('status') === '1' ? 'selected' : '' }}>未読</option>
@@ -134,8 +169,8 @@
                 <input type="submit" class="detail-search-btn-clear" value="検索をクリア">
             </div>
         </form>
-    </div>
-</section>
+    </div> --}}
+{{-- </section> --}}
 
 @if (Session::has('search'))
 <div class="h4 text-center mt-3">
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,6 +4,15 @@
 
         @if(Auth::check())
         <ul class="navbar-nav ml-auto">
+            <li class="navbar-item text-white mr-5">
+                <form method="POST" action="{{ route('books.index') }}" class="text-center">
+                    @csrf
+                    <div class="form-group m-0">
+                        <input type="text" name="keyword" value="{{ old('keyword') }}" placeholder="本棚からさがす">
+                        <input type="submit" value="検索">
+                    </div>
+                </form>
+            </li>
             {{--  <li class="navbar-item">
                 <a class="btn btn-lg btn-primary mx-2 shadow" href="">
                     本をさがす
@@ -14,23 +23,23 @@
                     本棚
                 </a>
             </li>  --}}
-            @if(Auth::user()->thumbnail)
-            <li class="navbar-item user-icon dropdown">
-                <img src="../storage/app/public/user/{{Auth::user()->thumbnail}}"
+            {{-- @if(Auth::user()->thumbnail) --}}
+            <li class="navbar-item dropdown">
+                <a
                 id="dropdown1"
-                class="dropdown-toggle"
+                class="dropdown-toggle h5 text-white pr-2"
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-                >
+                >Menu</a>
                 <div class="dropdown-menu dropdown-location" aria-labelledby="dropdown1">
                     <a class="dropdown-item" href="{{ route('books.index') }}">ホーム</a>
                     <a class="dropdown-item"  href="{{ route('user.edit', ['user' => Auth::user()]) }}">アカウント管理</a>
                     <button class="dropdown-item" form="logout-form" id="logout">ログアウト</button>
                 </div>
             </li>
-            @elseif(Auth::user()->thumbnail == null)
-            <li class="navbar-item user-icon dropdown">
+            {{-- @elseif(Auth::user()->thumbnail == null) --}}
+            {{-- <li class="navbar-item user-icon dropdown">
                 <img src="{{ asset('../storage/app/public/common/default_user.jpeg') }}" id="dropdown1"
                 class="dropdown-toggle"
                 data-toggle="dropdown"
@@ -42,8 +51,8 @@
                     <a class="dropdown-item"  href="{{ route('user.edit', ['user' => Auth::user()]) }}">アカウント管理</a>
                     <button class="dropdown-item" form="logout-form" id="logout">ログアウト</button>
                 </div>
-            </li>
-            @endif
+            </li> --}}
+            {{-- @endif --}}
         </ul>
 
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
