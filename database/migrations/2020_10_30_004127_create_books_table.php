f18fddec63f59e549e47f474091bd8f702fe391b
kb995 <t.kuboshima995@gmail.com>
2021-01-01 04:40:24 +0900

    [fix]published_date追加

--- database/migrations/2020_10_30_004127_create_books_table.php
+++ database/migrations/2020_10_30_004127_create_books_table.php
@@ -13,6 +13,7 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
+
         Schema::create('books', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title', 100);
