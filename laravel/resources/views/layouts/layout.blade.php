b0293a95d9cbdc6ca907ec2cf13a67a118cf85dd
kb995 <t.kuboshima995@gmail.com>
2021-01-29 23:08:44 +0900

    [fix]jsの微調整

--- laravel/resources/views/layouts/layout.blade.php
+++ laravel/resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>@yield('title')</title>
     <link href="{{ asset('./css/app.css' )}}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">{{-- toastr --}}
@@ -25,8 +26,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>{{-- jQuery --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>{{-- torstr --}}
 
-    <script src="{{ asset('/js/common.js') }}"></script>
     <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="{{ asset('/js/common.js') }}"></script>
 
 
     <script>
