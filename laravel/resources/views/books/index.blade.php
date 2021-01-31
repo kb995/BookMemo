5e467d4e5a17c58574d46a3a8d2d95bcf6588bf4
kb995 <t.kuboshima995@gmail.com>
2021-02-01 00:38:55 +0900

    [fix]カバー一覧画像調整

--- laravel/resources/views/books/index.blade.php
+++ laravel/resources/views/books/index.blade.php
@@ -76,17 +76,17 @@
         @foreach( $books as $book)
         <div class="col-md-4 col-sm-6 col-xs-12">
             <div class="book-item mb-5 shadow">
-                <div class="book-item-cover">
+                <div class="book-item-cover border shadow">
                     @if($book->img_url === null)
                     <div>
                         <a href="{{ route('books.show', ['book' => $book]) }}">
-                            <img class="shadow fadein" src="https://book-quote.s3-ap-northeast-1.amazonaws.com/layouts/default_cover.png">
+                            <img src="https://book-quote.s3-ap-northeast-1.amazonaws.com/layouts/default_cover.png">
                         </a>
                     </div>
                     @else
                     <div>
                         <a href="{{ route('books.show', ['book' => $book]) }}">
-                            <img class="shadow fadein"  src="{{ $book->img_url }}">
+                            <img src="{{ $book->img_url }}">
                         </a>
                     </div>
                     @endif
