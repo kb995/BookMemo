9baa8fa2a1943ac6a9635ec5146b35e278d204e3
kb995 <t.kuboshima995@gmail.com>
2021-02-13 05:32:04 +0900

    [fix]コンフリクト解消

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use App\User;
 use Illuminate\Http\Request;
@@ -38,6 +39,43 @@ class LoginController extends Controller
     /**
      * 外部ログイン
      */
+        //ログインボタンからリンク
+        // public function socialLogin($social)
+        // {
+        //     return Socialite::driver($social)->redirect();
+        // }
+
+        //Callback処理
+        // public function handleProviderCallback($social)
+        // {
+        //     //ソーシャルサービス（情報）を取得
+        //     $userSocial = Socialite::driver($social)->stateless()->user();
+        //     //emailで登録を調べる
+        //     $user = User::where(['email' => $userSocial->getEmail()])->first();
+
+        //     //登録（email）の有無で分岐
+        //     if($user){
+
+        //         //登録あればそのままログイン（2回目以降）
+        //         Auth::login($user);
+        //         return redirect('/books');
+
+        //     }else{
+
+        //         //なければ登録（初回）
+        //         $newuser = new User;
+        //         $newuser->name = $userSocial->getName();
+        //         $newuser->email = $userSocial->getEmail();
+        //         $newuser->save();
+
+        //         //そのままログイン
+        //         Auth::login($newuser);
+        //         return redirect('/books');
+
+        //     }
+        // }
+
+
     /**
      * Create a new controller instance.
      *
