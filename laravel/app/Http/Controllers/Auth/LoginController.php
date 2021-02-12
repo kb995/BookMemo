69d43f2898c6afe1836c26192b6651425a3f36ef
kb995 <t.kuboshima995@gmail.com>
2021-02-12 23:32:39 +0900

    [add]google_login

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -38,24 +38,24 @@ class LoginController extends Controller
     /**
      * 外部ログイン
      */
-    public function redirectToProvider(string $provider)
-    {
-        return Socialite::driver($provider)->redirect();
-    }
+    // public function redirectToProvider(string $provider)
+    // {
+    //     return Socialite::driver($provider)->redirect();
+    // }
 
-    public function handleProviderCallback(Request $request, string $provider)
-    {
-        $providerUser = Socialite::driver($provider)->stateless()->user();
+    // public function handleProviderCallback(Request $request, string $provider)
+    // {
+    //     $providerUser = Socialite::driver($provider)->stateless()->user();
 
-        $user = User::where('email', $providerUser->getEmail())->first();
+    //     $user = User::where('email', $providerUser->getEmail())->first();
 
-        if ($user) {
-            $this->guard()->login($user, true);
-            return $this->sendLoginResponse($request);
-        }
+    //     if ($user) {
+    //         $this->guard()->login($user, true);
+    //         return $this->sendLoginResponse($request);
+    //     }
 
         // $userがnullの場合の処理は次のパートでここに書く予定
-    }
+    // }
 
     /**
      * Create a new controller instance.
--- laravel/config/services.php
+++ laravel/config/services.php
@@ -30,10 +30,14 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    // 'google' => [
+    //     'client_id' => env('GOOGLE_CLIENT_ID'),
+    //     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    //     'redirect' => env('APP_URL') . '/login/google/callback',
+    // ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/google/callback',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
-
 ];
